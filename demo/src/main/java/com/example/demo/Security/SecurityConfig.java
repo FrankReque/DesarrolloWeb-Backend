fe567f524bd8b01cf4340cfc293998679cee5143
FrankReque <fdrvega080206@gmail.com>
2026-07-06 00:08:26 -0500

    Configuracion7

--- demo/src/main/java/com/example/demo/Security/SecurityConfig.java
+++ demo/src/main/java/com/example/demo/Security/SecurityConfig.java
@@ -34,9 +34,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
-        configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
         configuration.setExposedHeaders(List.of("Set-Cookie"));
 
