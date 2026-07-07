ccdf727a5b46581cc32fe97991dfa095f2fad535
FrankReque <fdrvega080206@gmail.com>
2026-07-06 23:36:07 -0500

    Configuracion18

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,8 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+
+@SpringBootApplication(
+    exclude = {UserDetailsServiceAutoConfiguration.class}  // ← Desactiva la autoconfiguración por defecto
+)
+@Import(com.example.demo.Security.SecurityConfig.class)
+@ComponentScan(basePackages = {"com.example.demo.Security"})
 public class DemoApplication {
 
 	public static void main(String[] args) {
